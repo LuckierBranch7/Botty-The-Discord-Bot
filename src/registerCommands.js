702ad377a1fdd90939e6132e70f15a70baf6a9d9
Milo M. Cruz <milo.cruz.2011@gmail.com>
2024-08-08 11:33:24 -0400

    Adding Base Commands

---
+++ src/registerCommands.js
@@ -0,0 +1,24 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+    {
+        name: 'hi',
+        description: 'Says hi',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log("Initializing commands");
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+        console.log("Commands are primed!");
+    } catch (error) {
+        console.log(`Error! ${error}`);
+    }
+})();
