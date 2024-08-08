680b05aeb2b9d746e3e8977fcdd8108f84c1c780
LuckierBranch7 <milo.cruz.2011@gmail.com>
2024-08-07 23:31:42 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// To run, put into the terminal: node src/index.js
+// You can also (only if you have nodemon globaly installed) put: nodemon
+
+// Unless your forking this project, you can remove line 5
+
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (cnt) => {
+    console.log(`${cnt.user.tag} is ready!`);
+});
+
+client.on("messageCreate", (msg) => {
+    if(msg.author.bot) {
+        return;
+    }
+
+    if(msg.content === 'hi botty') {f
+        msg.reply('Hello There!')
+    }
+});
+
+// If you removed line 5, you can replace process.env.TOKEN with your bot token
+client.login(process.env.TOKEN);
